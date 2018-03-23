86c66dd708c297e559b98346e620951b0ee25c16
sdeagh <huw.burton@btinternet.com>
2018-03-23 13:35:07 +0000

    removing env.json

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-const config = require('./env.json');
 const watson = require('watson-developer-cloud');
 const vcapServices = require('vcap_services');
 
