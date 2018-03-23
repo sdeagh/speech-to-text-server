4e111e3a04e2870a3a66348ef635663ffa88332c
sdeagh <huw.burton@btinternet.com>
2018-03-23 11:58:10 +0000

    adding PORT

--- index.js
+++ index.js
@@ -73,6 +73,6 @@ app.get('/play', (req, res, next) => {
 });
 
 
-app.listen(3000, function() {
-    console.log("Speech server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Speech server started on port ", process.env.PORT);
 });
